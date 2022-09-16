f6b870e0ab9cc98e9baa6c5942daec8313b8e146
Paul Courts <paulcourts@Pauls-Air.lan>
2022-09-16 17:51:08 -0500

    Rendering questions

--- play.js
+++ play.js
@@ -1,103 +1,167 @@
 const question = document.getElementById("question");
-const choices = array.from(document.getelementclassname("choice-text"));
-console.log(choices);
+const choices = document.querySelectorAll(".choice-text");
 
 
-let currentquetions = {};
-let acceptinganswers = true;
+let currentquetion = [];
+let acceptinganswers = false;
 let score = 0;
-let quetioncounter = 0;
-let availablequetions = [];
+let questioncounter = 0;
+let availablequestions = [
 
-let questions = [
     {
-        question: "insode is a question"
-        choice1: "<choise 1>"
-        choice2: "<choise 2>"
-        choice3: "<choise 3>"
-        choice4: "<choise 4>"
+        questions: "inside 1 is a question",
+        choice1: "<choise 1>",
+        choice2: "<choise 2>",
+        choice3: "<choise 3>",
+        choice4: "<choise 4>",
         answer: 1
-    }
+    },
 
     {
-        question: "insode is a question"
-        choice1: "<choise 1>"
-        choice2: "<choise 2>"
-        choice3: "<choise 3>"
-        choice4: "<choise 4>"
+        questions: "inside 2is a question",
+        choice1: "<choise 1>",
+        choice2: "<choise 2>",
+        choice3: "<choise 3>",
+        choice4: "<choise 4>",
         answer: 2
-    }
+    },
 
     {
-        question: "inside is a question"
-        choice1: "<choise 1>"
-        choice2: "<choise 2>"
-        choice3: "<choise 3>"
-        choice4: "<choise 4>"
+        questions: "inside3 is a question",
+        choice1: "<choise 1>",
+        choice2: "<choise 2>",
+        choice3: "<choise 3>",
+        choice4: "<choise 4>",
         answer: 3
-    }
+    },
 
     {
-        question: "insisde is a question"
-        choice1: "<choise 1>"
-        choice2: "<choise 2>"
-        choice3: "<choise 3>"
-        choice4: "<choise 4>"
+        questions: "inside 4is a question",
+        choice1: "<choise 1>",
+        choice2: "<choise 2>",
+        choice3: "<choise 3>",
+        choice4: "<choise 4>",
         answer: 4
-    }
+    },
 
     {
-        question: "inside is A question"
-        choice1: "<choise 1>"
-        choice2: "<choise 2>"
-        choice3: "<choise 3>"
-        choice4: ",choise 4>"
+        questions: "inside 5is A question",
+        choice1: "<choise 1>",
+        choice2: "<choise 2>",
+        choice3: "<choise 3>",
+        choice4: ",choise 4>",
         answer: 2
     }
 ];
 
 // constants
 
-const corecct_bonus = 10;
+const correct_bonus = 10;
 const max_questions = 3;
 
-satrtgame = () => {
-    qestionscounter = 0;
+startplay = () => {
+
+    questioncounter = 0;
     score = 0;
-    availablequetions = [... questions];
-    consolwe.log(availablequetions);
+
+    setTime();
     getnewquestion();
 };
-getnewquestion = () => {
-    
-    if(availablequetions.length === qestionscounter > max_questions)
-    //end page
-    return window.loaction.assign(end.page);
+//var
+
+
+var timeEl = document.querySelector(".time");
+
+// Selects element by id
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 15;
+
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " seconds left till next question.";
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+
+    }, 1000);
 }
-    questioncunter++;
-    const quesationindex = Math.floor(math.random() = availablequetions.length);
-    currentquetion = availablequetions[quesationindex];
-    question.innerText = currentquetion.question;
+
+//Function to create and question
+function sendMessage() {
+    timeEl.textContent = " ";
+
+}
+
+
+
+
+// function selectanswer(e) {
+//     const selectedButton = e.Target
+//     const Correct = selectedButton.dataset.Correct
+//     setstatusclass(button, button.dataset.correct)
+//     Array.from(answerbuttonsElement.children).forEach(button => {
+
+//     });
+
+
+// start quiz function
+//1 hide the start screen 2 unhide quetion screen 3 start timer , link that to the butten, 4 call the geit questions functions
+getnewquestion = () => {
+
+    // if (availablequestions.length === 0 || questioncounter > max_questions) {
+    //end page
+    // return window.location.assign("./end.html");
+    // }
+
+
+
+    question.innerText = availablequestions[questioncounter].questions;
 
     choices.forEach(choice => {
         const number = choice.dataset["number"];
-        choice.innerText = currentquetions["choice" = number];
+        switch (number) {
+            case "1":
+                choice.innerText = availablequestions[questioncounter].choice1;
+                break;
+            case "2":
+                choice.innerText = availablequestions[questioncounter].choice2;
+                break;
+            case "3":
+                choice.innerText = availablequestions[questioncounter].choice3;
+                break;
+            case "4":
+                choice.innerText = availablequestions[questioncounter].choice4;
+                break;
+        }
+
     });
 
-        availablequetions.splice(quesationindex, 1);
-        avaiableanswers = true;
 
-        choice.forEach(choice => {
-            choice.addeventlistener("click", e => {
-                if(!acceptinganswers) return;
 
-                acceptinganswers = false;
-                const selectedchoice = e.Target;
-                const selectanswer = selectedchoice.dataset("number");
-                console.log(E.Target);
-                getnewquestion();
-        });
+};
+
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        // if (!acceptinganswers) return;
+
+        // acceptinganswers = false;
+        const selectedchoice = e.target;
+        const selectedanswer = selectedchoice.dataset["number"];
+  
+        if (questioncounter < availablequestions.length - 1) {
+            questioncounter++
+            getnewquestion();
+        }
     });
+});
 
 
-startGame();
+
+
+
+startplay();
